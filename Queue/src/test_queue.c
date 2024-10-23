f2022a17651013e7f76b760e8b88c8fb4bb3cc32
moon <undruee@gmail.com>
2024-10-23 21:19:08 +0300

    test insert

--- Queue/src/test_queue.c
+++ Queue/src/test_queue.c
@@ -16,12 +16,65 @@ START_TEST(createQueueLarge) {
 }
 END_TEST
 
+START_TEST(insertQueueNormal) {
+  Queue_T *queue = createQueue(89);
+  insertQueue(queue, 12);
+  insertQueue(queue, 77);
+  insertQueue(queue, 54);
+  int one, two, three;
+  queue = pop(queue, &one);
+  queue = pop(queue, &two);
+  queue = pop(queue, &three);
+  _ck_assert_int(one, ==, 89);
+  _ck_assert_int(two, ==, 12);
+  _ck_assert_int(three, ==, 77);
+  _ck_assert_int(queue->variable, ==, 54);
+  _ck_assert_ptr(queue, !=, NULL);
+  removeQueue(queue);
+}
+END_TEST
+
+START_TEST(insertQueueIsNotQueue) {
+  Queue_T *queue = NULL;
+  _ck_assert_int(insertQueue(queue, 43), ==, ERROR);
+  _ck_assert_int(insertQueue(queue, 54), ==, ERROR);
+  _ck_assert_int(insertQueue(queue, 100000000), ==, ERROR);
+  if (queue) {
+    removeQueue(queue);
+  }
+}
+END_TEST
+
+START_TEST(insertQueueLarge) {
+  Queue_T *queue = createQueue(348764271);
+  insertQueue(queue, 1000000000);
+  insertQueue(queue, 632535155);
+  insertQueue(queue, 859180499);
+  int one, two, three;
+  queue = pop(queue, &one);
+  queue = pop(queue, &two);
+  queue = pop(queue, &three);
+  _ck_assert_int(one, ==, 348764271);
+  _ck_assert_int(two, ==, 1000000000);
+  _ck_assert_int(three, ==, 632535155);
+  _ck_assert_int(queue->variable, ==, 859180499);
+  _ck_assert_ptr(queue, !=, NULL);
+  removeQueue(queue);
+}
+END_TEST
+
 Suite *QueueTest(void) {
   Suite *suite = suite_create("Queue");
   TCase *tcase = tcase_create("Queue");
 
+  // Тесты создания очереди
   tcase_add_test(tcase, createQueueNormal);
   tcase_add_test(tcase, createQueueLarge);
+
+  // Тесты добавления элемента в очередь
+  tcase_add_test(tcase, insertQueueNormal);
+  tcase_add_test(tcase, insertQueueIsNotQueue);
+  tcase_add_test(tcase, insertQueueLarge);
 
   suite_add_tcase(suite, tcase);
   return suite;
--- Queue/src/test_queue.h
+++ Queue/src/test_queue.h
@@ -1,6 +1,7 @@
 #ifndef TEST_QUEUE_H
 #define TEST_QUEUE_H
 #include <check.h>
-
+#define OK 1
+#define ERROR 0
 #include "queue.h"
 #endif
