7fe126facd3b668c9cd932d25a70fb7de98081e9
moon <undruee@gmail.com>
2024-10-23 09:02:05 +0300

    Bb

---
+++ Binary_branch/src/binary.c
@@ -0,0 +1,69 @@
+#include "binary.h"
+
+struct binary* init(int var){
+    struct binary* root = (struct binary*)malloc(sizeof(struct binary));
+    if(root!=NULL){
+        root->variable=var;
+        root->max=NULL;
+        root->min=NULL;
+    }
+    return root;
+}
+
+struct binary* add(struct binary* root, int var){
+    int trig=0;
+    struct binary* root_cp=root;
+    struct binary* line=(root_cp->variable>var) ? root_cp->min : root_cp->max;
+    while(line!=NULL){
+        root_cp=line;
+        if(root_cp->variable>var){
+            line=root_cp->min;
+        }
+        if(root_cp->variable<var){
+            line=root_cp->max;
+        }
+        if(root_cp->variable==var){
+            trig=1;
+            break;
+        }
+    }
+    if(root_cp->variable>var && trig==0){
+        root_cp->min=init(var);
+        line=root_cp->min;
+    }
+    if(root_cp->variable<var && trig==0){
+        root_cp->max=init(var);
+        line=root_cp->max;
+    }
+    return line;
+}
+
+void print_recursively(struct binary* root) {
+    if(root == NULL){
+        exit(1);
+    }
+    else {
+        printf("%d ", root->variable);
+        if(root->min!=NULL){
+            print_recursively(root->min);
+        }
+        if(root->max!=NULL){
+            print_recursively(root->max);
+        }
+    }
+}
+
+void print_sort(struct binary* root){
+    if(root!=NULL){
+        print_sort(root->min);
+        printf("%d ", root->variable);
+        print_sort(root->max);
+    }
+}
+
+void delete_tree(struct binary* root) {
+    if (root==NULL) return;
+    delete_tree(root->min);
+    delete_tree(root->max);
+    free(root);
+}
---
+++ Binary_branch/src/binary.h
@@ -0,0 +1,15 @@
+#ifndef BINARY_H
+#define BINARY_H
+#include <stdio.h>
+#include <stdlib.h>
+struct binary {
+    int variable;
+    struct binary* max;
+    struct binary* min;
+};
+struct binary* init(int);
+struct binary* add(struct binary*, int);
+void print_recursively(struct binary*);
+void print_sort(struct binary*);
+void delete_tree(struct binary*);
+#endif
